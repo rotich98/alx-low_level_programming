32024df19aa444700f6078f910ab395dcdf596bd
rotich98 <emmanuelkipkemei13@gmail.com>
2022-06-30 11:45:09 -0700

    Correction task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,18 @@
-#!/bin/bash
 #include <stdio.h>
 
+/**
+ *
+ * Explanation: main - it prints out a string to stdout.
+ *
+ * Return: 0 if success.
+ */
+
 int main(void)
+
 {
+
 	puts("\"Programming is like building a multilingual puzzle");
+
 	return (0);
+
 }
