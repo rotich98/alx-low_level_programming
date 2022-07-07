4aa2d1a39932636a0002b158f2c3e037a3d19270
rotich98 <emmanuelkipkemei13@gmail.com>
2022-07-07 11:15:42 -0700

    Task 2:A function that multiply two intergers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - multiply two numbers
+ * @a: interger parameter 1
+ * @b: interger parameter 2
+ * Return: Success and integer
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
