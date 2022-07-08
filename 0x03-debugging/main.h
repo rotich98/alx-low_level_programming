ae308a1e3fc0bb531a2caa68a5417f14db84e6d3
rotich98 <emmanuelkipkemei13@gmail.com>
2022-07-08 01:56:02 -0700

    Fixing error

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -4,8 +4,6 @@
 #include <stdio.h>
 
 void positive_or_negative(int i);
-void print_remaining_days(int month, int day, int year);
-int convert_day(int month, int day);
-void print_times_table(int n);
 
-#endif
+
+#endif /* MAIN_H */
