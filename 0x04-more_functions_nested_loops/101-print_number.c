2f0464bd6f0da1f1402845cd67e04a6d2066d8fd
rotich98 <emmanuelkipkemei13@gmail.com>
2022-07-08 12:34:00 -0700

    hot fix

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -16,10 +16,10 @@ void print_numbers(int i)
 		_putchar('-');
 	}
 
-	i /= 10;
+	x /= 10;
 
-	if (i != 0)
-		print_number(i);
+	if (x != 0)
+		print_number(x);
 	_putchar((unsigned int) i % 10 + '0');
 }
 
