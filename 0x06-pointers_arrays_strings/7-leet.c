b635910bf82175fa8d775f602617668f82e4075d
rotich98 <emmanuelkipkemei13@gmail.com>
2022-07-14 05:35:26 -0700

    A function that encodes a string

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * leet -> encodes a string
+ * @p: The string to encode
+ * Return: The encoded string
+ */
+char *leet(char *p)
+{
+	int len1 = 0, len2;
+	char leet[8] = {'O', 'L', '?', 'E', 'A', '?', '?', 'T'};
+
+	while (p[++len1])
+	{
+		for (len2 = 0; len2 <= 7; len2++)
+		{
+			if (p[len1] == leet[len2] || p[len1] - 32 == leet[len2])
+				p[len1] = len2 + '0';
+		}
+	}
+	return (p);
+}
+
