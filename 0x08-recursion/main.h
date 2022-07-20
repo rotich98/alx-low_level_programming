71339753defede05b4f1b0b20fb03298c7196681
rotich98 <emmanuelkipkemei13@gmail.com>
2022-07-20 08:00:12 -0700

    hot fix

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,9 +1,11 @@
 #ifndef MAIN_H
 #define MAIN_H
-int _putchar(char c);
+int _putchar(char c)i;
+int mathsquare(int n, int corr);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
+int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 
