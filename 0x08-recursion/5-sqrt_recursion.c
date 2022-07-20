33e424c95b3f0185fc07216101a3b965dfa4cab0
rotich98 <emmanuelkipkemei13@gmail.com>
2022-07-20 07:54:15 -0700

    hot fix

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -7,21 +7,21 @@
  */
 int _sqrt_recursion(int n)
 {
-	return (math_square(n, 1));
+	return (mathsquare(n, 1));
 }
 
 /**
- * math_square -> find square root
+ * mathsquare -> find square root
  * @n: value int to find square root
  * @corr: Square root
  * Return: int
  */
-int math_square(int n, int corr)
+int mathsquare(int n, int corr)
 {
 	if (corr * corr == n)
 		return (corr);
 	else if (corr * corr < n)
-		return (math_square(n, corr + 1));
+		return (mathsquare(n, corr + 1));
 	else
 		return (-1);
 }
