5796d53b52984cbd78ecc2dded582e27d3f28127
rotich98 <emmanuelkipkemei13@gmail.com>
2022-07-22 14:18:57 -0700

    hotfix

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -9,15 +9,16 @@
  * @argv: Array of argument strings
  * Return: 0
  */
-int main(int argc, char *argv[])
+int main(int argc, char *argv)
 {
-	if (argc < 2)
+	int item;
+
+	if (argc != 3)
 	{
 		printf("Error\n");
+		return (1);
 	}
-	else
-	{
-		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
-	}
+	product = atoi(argv[1]) * atoi(argv[2]);
+	printf("%d\n", item);
 	return (0);
 }
