1910f51d66216d8411099bd6daa293df21dc04b8
rotich98 <emmanuelkipkemei13@gmail.com>
2022-08-04 11:31:14 -0700

    hotfix

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -27,6 +27,6 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		printf("%s%s", c == NULL ? "(nil)" : c, a != (n - 1) ? separator : "");
 	}
 
-	printf('\n');
 	va_end(ap);
+	putchar('\n');
 }
