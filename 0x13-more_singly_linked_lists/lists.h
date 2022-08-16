fe730f95463c335d46acbd6ee25213cfcde5dc8c
rotich98 <emmanuelkipkemei13@gmail.com>
2022-08-16 13:17:47 -0700

    task 10

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -32,5 +32,6 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
 int sum_listint(listint_t *head);
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
 void sumofnodes(listint_t *head, int *sum);
+int delete_nodeint_at_index(listint_t **head, unsigned int index);
 
 #endif
